8460357f45406130259974f83751a8cd43e7ee0e
didiyudha <didiyudha@gmail.com>
2020-05-06 04:30:07 +0700

    add user model

--- domain/user/model/model.go
+++ domain/user/model/model.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type User struct {
+	ID uuid.UUID `json:"id"`
+	Username string `json:"username"`
+	Email string `json:"email"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
