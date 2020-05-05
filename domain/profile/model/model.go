2537d7c4e7cbe7098043eae27c3b95a931b2d9a3
didiyudha <didiyudha@gmail.com>
2020-05-06 04:25:30 +0700

    add profile model

--- domain/profile/model/model.go
+++ domain/profile/model/model.go
@@ -1,1 +1,17 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Profile struct {
+	ID        uuid.UUID  `json:"id"`
+	UserID    uuid.UUID  `json:"user_id"`
+	FirstName string     `json:"first_name"`
+	LastName  string     `json:"last_name"`
+	Address   string     `json:"address"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
