012bb659089db3a8044fdff9153ee1bc4abdddc3
didiyudha <didiyudha@gmail.com>
2020-05-06 02:05:34 +0700

    load configuration in main function

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/didiyudha/transaction-accross-pkg/config"
+	"github.com/didiyudha/transaction-accross-pkg/internal/platform/postgres"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	if err := config.Load(); err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Infof("%+v\n", config.Cfg)
+	_, err := postgres.Open(config.Cfg.DB)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Infof("successfully connected to postgres")
 }
