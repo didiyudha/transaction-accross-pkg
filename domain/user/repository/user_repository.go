fde409b84dc4fc6fd72a9401b2625b4d7234c816
didiyudha <didiyudha@gmail.com>
2020-05-06 04:37:22 +0700

    add user repository impl

--- domain/user/repository/user_repository.go
+++ domain/user/repository/user_repository.go
@@ -1,1 +1,18 @@
 package repository
+
+import (
+	"context"
+	"github.com/didiyudha/transaction-accross-pkg/domain/user/model"
+)
+
+func (u *userRepository) Save(ctx context.Context, user *model.User) error {
+	db := u.GetDB(true)
+	query := `INSERT INTO users (id, username, email, created_at, updated_at, deleted_at) 
+				VALUES ($1, $2, $3, $4, $5, $6)`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(user.ID, user.Username, user.Email, user.CreatedAt, user.UpdatedAt, user.DeletedAt)
+	return err
+}
