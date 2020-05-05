ef4e65ee2190e8b7f844ee81fd24c46e54c88e28
didiyudha <didiyudha@gmail.com>
2020-05-06 05:19:59 +0700

    create http handler to create profile

--- domain/profile/handler/create_profile_handler.go
+++ domain/profile/handler/create_profile_handler.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"github.com/didiyudha/transaction-accross-pkg/domain/profile/model"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func (h *ProfileHandler) CreateProfile(c echo.Context) error {
+	var payload model.CreateProfileReq
+	if err := c.Bind(&payload); err != nil {
+		return err
+	}
+	ctx := c.Request().Context()
+	profileDetail, err := h.ProfileUseCase.Save(ctx, payload)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, profileDetail)
+}
--- domain/profile/handler/handler.go
+++ domain/profile/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/didiyudha/transaction-accross-pkg/domain/profile/usecase"
+
+type ProfileHandler struct {
+	ProfileUseCase usecase.ProfileUseCase
+}
+
+func NewProfileHandler(profileUseCase usecase.ProfileUseCase) *ProfileHandler {
+	return &ProfileHandler{
+		ProfileUseCase:profileUseCase,
+	}
+}
