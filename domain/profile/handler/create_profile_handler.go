d4f2e8c1688fc6d59cab18ca77152d681e697481
didiyudha <didiyudha@gmail.com>
2020-05-06 09:36:06 +0700

    add find by id handler

--- domain/profile/handler/create_profile_handler.go
+++ domain/profile/handler/create_profile_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"github.com/didiyudha/transaction-accross-pkg/domain/profile/model"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
@@ -17,4 +18,22 @@ func (h *ProfileHandler) CreateProfile(c echo.Context) error {
 		return err
 	}
 	return c.JSON(http.StatusCreated, profileDetail)
+}
+
+func (h *ProfileHandler) FindByID(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	paramID := c.Param("id")
+	id, err := uuid.Parse(paramID)
+	if err != nil {
+		return err
+	}
+	profileDetail, err := h.ProfileUseCase.FindByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	if profileDetail.ID == uuid.Nil {
+		return echo.NewHTTPError(http.StatusNotFound, http.StatusText(http.StatusNotFound))
+	}
+	c.JSON(http.StatusOK, profileDetail)
 }
