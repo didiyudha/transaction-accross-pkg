8d2ed8a18f80b922e6052c044e6a21c3a1c695c6
didiyudha <didiyudha@gmail.com>
2020-05-06 09:35:51 +0700

    add find by id usecase

--- domain/profile/usecase/usecase.go
+++ domain/profile/usecase/usecase.go
@@ -5,10 +5,12 @@ import (
 	"github.com/didiyudha/transaction-accross-pkg/domain/profile/model"
 	"github.com/didiyudha/transaction-accross-pkg/domain/profile/repository"
 	userrepo "github.com/didiyudha/transaction-accross-pkg/domain/user/repository"
+	"github.com/google/uuid"
 )
 
 type ProfileUseCase interface {
 	Save(ctx context.Context, req model.CreateProfileReq) (*model.ProfileDetail, error)
+	FindByID(ctx context.Context, id uuid.UUID) (*model.ProfileDetail, error)
 }
 
 type profileUseCase struct {
