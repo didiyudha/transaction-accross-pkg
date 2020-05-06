64bbf006050eded67e4b7fc856ca71762f6c8497
didiyudha <didiyudha@gmail.com>
2020-05-06 09:49:53 +0700

    inject dependency start echo server

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/didiyudha/transaction-accross-pkg/config"
+	"github.com/didiyudha/transaction-accross-pkg/domain/profile/handler"
+	"github.com/didiyudha/transaction-accross-pkg/domain/profile/repository"
+	"github.com/didiyudha/transaction-accross-pkg/domain/profile/usecase"
+	userrepo "github.com/didiyudha/transaction-accross-pkg/domain/user/repository"
 	"github.com/didiyudha/transaction-accross-pkg/internal/platform/postgres"
+	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
 )
 
@@ -10,10 +16,23 @@ func main() {
 	if err := config.Load(); err != nil {
 		logrus.Fatal(err)
 	}
-	logrus.Infof("%+v\n", config.Cfg)
-	_, err := postgres.Open(config.Cfg.DB)
+	dbWrite, err := postgres.Open(config.Cfg.DB)
 	if err != nil {
 		logrus.Fatal(err)
 	}
-	logrus.Infof("successfully connected to postgres")
+	dbRead, err := postgres.Open(config.Cfg.DB)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	userRepository := userrepo.NewUserRepository(dbWrite, dbRead)
+	profileRepository := repository.NewProfileRepository(dbWrite, dbRead)
+	profileUseCase := usecase.NewProfileUseCase(profileRepository, userRepository)
+	profileHandler := handler.NewProfileHandler(profileUseCase)
+
+	e := echo.New()
+	e.POST("/profiles", profileHandler.CreateProfile)
+	e.GET("/profiles/:id", profileHandler.FindByID)
+
+	port := fmt.Sprintf(":%d", config.Cfg.Port)
+	e.Logger.Fatal(e.Start(port))
 }
